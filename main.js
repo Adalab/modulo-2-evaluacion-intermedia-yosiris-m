600e7dfdbe58e478a7343a00f790945633dec69a
Yosiris Mariñez <yosiris0510@gmail.com>
2021-09-29 15:12:08 +0200

    added styles

--- main.js
+++ main.js
@@ -4,36 +4,38 @@ const inputNumber = document.querySelector(".inputNumber");
 const btnTest = document.querySelector(".btnTest");
 const clue = document.querySelector(".clue");
 const spanCounter = document.querySelector(".counter");
-let counter = 0;
 
+let counter = 0;
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
 const generatedNumber = getRandomNumber(100);
 console.log(generatedNumber);
 
+function clueCount(msg) {
+  clue.innerHTML = msg;
+}
+
+function counterCount() {
+  counter += 1;
+  spanCounter.innerHTML = counter;
+}
 
 function onBtnTestClick(ev) {
-    ev.preventDefault();
-    
-    counter += 1;
-    console.log(`counter = ${counter}`);
-    spanCounter.innerHTML = counter;
+  ev.preventDefault();
+  counterCount();
 
-    const num = inputNumber.value;
-    console.log(`num = ${num}`);
-    
-    if (num >100 || num < 1){
-        clue.innerHTML = "El número debe estar entre 1 y 100";
-    } else if (num < generatedNumber) {
-        clue.innerHTML = "Demasiado bajo";
-    } else if (num > generatedNumber) {
-        clue.innerHTML = "Demasiado alto";
-    } else if (num == generatedNumber) {
-        clue.innerHTML = "Has ganado campeona!!!";
-    }
+  const num = inputNumber.value;
+  if (num > 100 || num < 1) {
+    clueCount("El número debe estar entre 1 y 100");
+  } else if (num < generatedNumber) {
+    clueCount("Demasiado bajo");
+  } else if (num > generatedNumber) {
+    clueCount("Demasiado alto");
+  } else if (num == generatedNumber) {
+    clueCount("Has ganado campeona!!!");
+  }
 }
 
 btnTest.addEventListener("click", onBtnTestClick);
-
